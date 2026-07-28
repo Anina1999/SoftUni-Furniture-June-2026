f6d9bec93d40bb09e9a0bf4f058b2158ec5c93b7
Anina Angelova <anina_1999@abv.bg>
2026-07-28 11:34:42 +0300

    Invalidate user session

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -3,6 +3,7 @@ import { userSchema } from "../schemas/userSchema.js";
 import { userService } from "../services/index.js";
 import { generateAuthToken } from "../utils/tokenUtils.js";
 import { getErrorMessage } from "../utils/errorUtils.js";
+import { addInvalidToken } from "../utils/invalidTokens.js";
 
 export async function register(req, res) {
     try {
@@ -38,5 +39,13 @@ export async function login(req, res) {
 }
 
 export async function logout(req, res) {
+    const token = req.headers['x-authorization'];
+
+    if (!token) {
+        return res.status(400).json({ error: 'No token provided'});
+    }
+
+    addInvalidToken(token);
+    
     res.json({ message: 'Logout successful' });
 }
